9fec506741d0898b601e5bf3525e43cb6c882164
Carlos André Lopes Santos Rosa <csrosa.ex@bancomontepio.pt>
2025-05-15 19:27:11 +0100

    fix: updated bundler settings

--- packages/renderer/rsbuild.config.ts
+++ packages/renderer/rsbuild.config.ts
@@ -3,6 +3,9 @@ import { pluginReact } from '@rsbuild/plugin-react';
 import { pluginSass } from '@rsbuild/plugin-sass';
 
 export default defineConfig({
+  html: {
+    title: 'Electron React App',
+  },
   plugins: [pluginReact(), pluginSass()],
   server: {
     port: 3000,
